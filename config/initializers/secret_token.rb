2b5cfc17c1ff7bc4fe552d23e97e427ce72076b8
abe <abe.schonfeld@gmail.com>
2013-12-11 12:08:42 -0500

    initial commit created abemailer

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AbeMail::Application.config.secret_token = 'dd304d9b0b60ba57b134c177034aaf68c43728f228c189998a9e4f1b4fd51d6ab3432612d120a052ad4ddeb1e27a7a69e01e506c32ded788fca91570e7f0b260'
