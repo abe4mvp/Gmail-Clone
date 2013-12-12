3aa9f8a214fc3f113b57a38b2ab0acb6ea49c17b
abe <abe.schonfeld@gmail.com>
2013-12-12 14:47:44 -0500

    Removes sendgrid stuff from env.rb

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ end
 
 module AbeMail
   class Application < Rails::Application
+    #needed for heroku
+    config.assets.initialize_on_precompile = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
--- config/environment.rb
+++ config/environment.rb
@@ -6,16 +6,16 @@ AbeMail::Application.initialize!
 
 if Rails.env.production?
   # only send real emails in production; use Sengrid
-  ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
-    :authentication => :plain,
-    :user_name      => abe4mvp,
-    #:password       => ,
-    :domain         => 'abemail.net',
-    :enable_starttls_auto => true # from sendgrid website
-  }
-  ActionMailer::Base.delivery_method ||= :smtp
+  # ActionMailer::Base.smtp_settings = {
+  #   :address        => 'smtp.sendgrid.net',
+  #   :port           => '587',
+  #   :authentication => :plain,
+  #   :user_name      => 'abe4mvp',
+  #   #:password       => ,
+  #   :domain         => 'abemail.net',
+  #   :enable_starttls_auto => true # from sendgrid website
+  # }
+  # ActionMailer::Base.delivery_method ||= :smtp
 elsif Rails.env.development?
 
   ActionMailer::Base.delivery_method = :letter_opener
