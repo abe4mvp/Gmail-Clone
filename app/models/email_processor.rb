865a3a3f25161ea16d42b0e4a528d686ba1d3612
abe <abe.schonfeld@gmail.com>
2013-12-13 16:09:53 -0500

    email cant be converted to string, revised the log statements..... again

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -1,6 +1,7 @@
 
 class EmailProcessor < ActiveRecord::Base # to add the logger
   def self.process(email)
+    logger.debug "--------------------started from the bottom now we here!"
  message = Message.new({
       body: email.raw_body,
       sender: email.from ,
@@ -11,7 +12,7 @@ class EmailProcessor < ActiveRecord::Base # to add the logger
 
     logger.debug "--------------------we made it this far!"
     message.save
-    logger.debug "X-----------------------------X" + message
+    ogger.debug "--------------------by now we might have an email!"
   end
 
 end
