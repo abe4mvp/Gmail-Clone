c793e0d54bf1d7e359ba3ccc2cc50b88a61491c8
abe <abe.schonfeld@gmail.com>
2013-12-17 17:30:23 -0500

    backbone now updates for the mail box to sent, need to add helper methods to display the message preview and timestamp

--- app/assets/javascripts/routers/emails_router.js
+++ app/assets/javascripts/routers/emails_router.js
@@ -37,7 +37,7 @@ AbeMail.Routers.Emails = Backbone.Router.extend({
   _swapView: function (newView) {
     this._currentView && this._currentView.remove();
     this._currentView = newView;
-    this.$rootEl.html(newView.render().$el);
+    AbeMail.$mailBox.html(newView.render().$el);
   }
 
 
