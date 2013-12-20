fbf025e6de22b9c41318bf93c2f23bcbc86a67b0
abe <abe.schonfeld@gmail.com>
2013-12-19 22:29:45 -0500

    abechat functional

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,12 +1,17 @@
 class ChatsController < ApplicationController
+  respond_to :json
 
   def create
+
     chat = params[:chat_text]
+    
     chat_partial = render_to_string(partial: "chats/chat", locals: {chat: chat})
-    Pusher.trigger("Abe", "new_chat_message", chat_partial)
+    Pusher.trigger(params[:chat_to], "new_chat_message", chat_partial)
+    
     if request.xhr?
       head :created
     else
+      fail
       redirect_to root_url
     end
   end
