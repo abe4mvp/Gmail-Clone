e51572d3cbabb72c6f137e97efcee2bddf038429
abe <abe.schonfeld@gmail.com>
2013-12-20 16:32:59 -0500

    put statements in chat controller?;

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -3,15 +3,21 @@ class ChatsController < ApplicationController
 
   def create
 
+
+
     chat = params[:chat_text]
-    
+
     chat_partial = render_to_string(partial: "chats/chat", locals: {chat: chat})
+
+    puts "chat text is nil" if params[:chat_text].nil?
+    puts "chat partial is nil" if chat_partial.nil?
+    puts "chat to is nil" if params[:chat_to].nil?
+
     Pusher.trigger(params[:chat_to], "new_chat_message", chat_partial)
-    
+
     if request.xhr?
       head :created
     else
-      fail
       redirect_to root_url
     end
   end
