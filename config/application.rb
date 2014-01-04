2b744e9732ce21609c0e99f369d359070a22d409
Abe Schonfeld <abe.schonfeld@gmail.com>
2014-01-03 20:56:44 -0500

    add .js in include tag with precompile of abechat

--- config/application.rb
+++ config/application.rb
@@ -61,7 +61,7 @@ module AbeMail
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-
+    config.assets.precompile << 'abeChat.js'
 
     config.assets.paths << "app/assets/templates"
 
