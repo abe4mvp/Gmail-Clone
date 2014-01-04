10f24b57cc5ee26dca3adc7c7a27cb10a89ea182
Abe Schonfeld <abe.schonfeld@gmail.com>
2014-01-03 20:00:49 -0500

    remove require tree and move chat script

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,11 +15,10 @@
 //= require underscore
 //= require backbone
 //= require jquery.ui.all
-//= require autocomplete-rails
 //= require abe_mail
 //= require_tree ../templates
 //= require_tree ./models
 //= require_tree ./collections
 //= require_tree ./views
 //= require_tree ./routers
-//= require_tree .
+
